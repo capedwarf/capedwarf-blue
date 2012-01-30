a9ea8832901bc92fcf3f9155250fb562825e4f2b
Marko Luksa <marko.luksa@gmail.com>
2012-01-30 12:49:08 +0100

    CAPEDWARF-8 Admin console

---
+++ admin/src/main/java/org/jboss/capedwarf/admin/DatastoreEntityViewer.java
@@ -0,0 +1,64 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.capedwarf.admin;
+
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.KeyFactory;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.ServletRequest;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ */
+@Named("datastoreEntity")
+@RequestScoped
+public class DatastoreEntityViewer {
+
+    public String getKey() {
+        ServletRequest request = (ServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        return request.getParameter("key");
+    }
+
+    public String getReadableKey() {
+        return KeyFactory.stringToKey(getKey()).toString();
+    }
+
+    public List<Map.Entry<String, Object>> getProperties() {
+        try {
+            Entity entity = DatastoreServiceFactory.getDatastoreService().get(KeyFactory.stringToKey(getKey()));
+            return new ArrayList<Map.Entry<String, Object>>(entity.getProperties().entrySet());
+        } catch (EntityNotFoundException e) {
+            return Collections.emptyList();
+        }
+    }
+
+}
