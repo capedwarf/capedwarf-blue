1ea766c55310b96e45ae8c189bd812f7a571dd65
Ales Justin <ales.justin@gmail.com>
2012-03-27 02:12:36 +0200

    Split tests.

--- tasks/src/test/java/org/jboss/test/capedwarf/tasks/test/PullTestCase.java
+++ tasks/src/test/java/org/jboss/test/capedwarf/tasks/test/PullTestCase.java
@@ -22,8 +22,12 @@
 
 package org.jboss.test.capedwarf.tasks.test;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskHandle;
 import com.google.appengine.api.taskqueue.TaskOptions;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -31,8 +35,7 @@ import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.test.capedwarf.tasks.support.PrintListener;
-import org.jboss.test.capedwarf.tasks.support.PrintServlet;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -40,61 +43,31 @@ import org.junit.runner.RunWith;
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
 @RunWith(Arquillian.class)
-public class TasksTestCase {
-    private static final String URL = "/_ah/test";
-    private static final String WEB_XML =
-                    "<web>" +
-                    " <listener>" +
-                    "  <listener-class>" + PrintListener.class.getName() + "</listener-class>" +
-                    " </listener>" +
-                    " <servlet>" +
-                    "  <servlet-name>PrintServlet</servlet-name>" +
-                    "  <servlet-class>" + PrintServlet.class.getName() + "</servlet-class>" +
-                    " </servlet>" +
-                    " <servlet-mapping>" +
-                    "  <servlet-name>PrintServlet</servlet-name>" +
-                    "  <url-pattern>" + URL + "</url-pattern>" +
-                    " </servlet-mapping>" +
-                    "</web>";
-
-    // we wait for JMS to kick-in
-    private static void sleep() throws InterruptedException {
-        Thread.sleep(3000L); // sleep for 3secs
-    }
-
+public class PullTestCase {
     @Deployment
     public static Archive getDeployment() {
         return ShrinkWrap.create(WebArchive.class)
-                .addClasses(PrintServlet.class, PrintListener.class)
-                .setWebXML(new StringAsset(WEB_XML))
+                .setWebXML(new StringAsset("<web/>"))
                 .addAsWebInfResource("appengine-web.xml");
     }
 
     @Test
-    public void testSmoke() throws Exception {
+    public void testPull() throws Exception {
         final Queue queue = QueueFactory.getQueue("default");
-        queue.add(TaskOptions.Builder.withUrl(URL));
-        sleep();
+        TaskHandle th = queue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).param("foo", "bar").payload("foobar").etaMillis(15000));
+        List<TaskHandle> handles = queue.leaseTasks(30, TimeUnit.MINUTES, 100);
+        Assert.assertFalse(handles.isEmpty());
+        TaskHandle lh = handles.get(0);
+        Assert.assertEquals(th.getName(), lh.getName());
     }
 
     @Test
-    public void testPayload() throws Exception {
+    public void testPullWithTag() throws Exception {
         final Queue queue = QueueFactory.getQueue("default");
-        queue.add(TaskOptions.Builder.withPayload("payload").url(URL));
-        sleep();
-    }
-
-    @Test
-    public void testHeaders() throws Exception {
-        final Queue queue = QueueFactory.getQueue("default");
-        queue.add(TaskOptions.Builder.withHeader("header_key", "header_value").url(URL));
-        sleep();
-    }
-
-    @Test
-    public void testParams() throws Exception {
-        final Queue queue = QueueFactory.getQueue("default");
-        queue.add(TaskOptions.Builder.withParam("param_key", "param_value").url(URL));
-        sleep();
+        TaskHandle th = queue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).tag("barfoo").payload("foobar").etaMillis(15000));
+        List<TaskHandle> handles = queue.leaseTasksByTag(30, TimeUnit.MINUTES, 100, "barfoo");
+        Assert.assertFalse(handles.isEmpty());
+        TaskHandle lh = handles.get(0);
+        Assert.assertEquals(th.getName(), lh.getName());
     }
 }
