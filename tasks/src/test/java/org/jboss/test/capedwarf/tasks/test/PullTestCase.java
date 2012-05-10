bc4c98b4fe2c71e66b008f02a04b1f11111e66ad
Ales Justin <ales.justin@gmail.com>
2012-05-11 00:04:12 +0200

    Cleanup after tests.

--- tasks/src/test/java/org/jboss/test/capedwarf/tasks/test/PullTestCase.java
+++ tasks/src/test/java/org/jboss/test/capedwarf/tasks/test/PullTestCase.java
@@ -56,65 +56,84 @@ public class PullTestCase {
     public void testPull() throws Exception {
         final Queue queue = QueueFactory.getQueue("default");
         TaskHandle th = queue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).param("foo", "bar").payload("foobar").etaMillis(15000));
-        List<TaskHandle> handles = queue.leaseTasks(30, TimeUnit.MINUTES, 100);
-        Assert.assertFalse(handles.isEmpty());
-        TaskHandle lh = handles.get(0);
-        Assert.assertEquals(th.getName(), lh.getName());
+        try {
+            List<TaskHandle> handles = queue.leaseTasks(30, TimeUnit.MINUTES, 100);
+            Assert.assertFalse(handles.isEmpty());
+            TaskHandle lh = handles.get(0);
+            Assert.assertEquals(th.getName(), lh.getName());
+        } finally {
+            queue.deleteTask(th);
+        }
     }
 
     @Test
     public void testPullWithTag() throws Exception {
         final Queue queue = QueueFactory.getQueue("default");
         TaskHandle th = queue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).tag("barfoo1").payload("foobar").etaMillis(15000));
-        List<TaskHandle> handles = queue.leaseTasksByTag(30, TimeUnit.MINUTES, 100, "barfoo1");
-        Assert.assertFalse(handles.isEmpty());
-        TaskHandle lh = handles.get(0);
-        Assert.assertEquals(th.getName(), lh.getName());
+        try {
+            List<TaskHandle> handles = queue.leaseTasksByTag(30, TimeUnit.MINUTES, 100, "barfoo1");
+            Assert.assertFalse(handles.isEmpty());
+            TaskHandle lh = handles.get(0);
+            Assert.assertEquals(th.getName(), lh.getName());
+        } finally {
+            queue.deleteTask(th);
+        }
     }
 
     @Test
     public void testPullMultipleWithSameTag() throws Exception {
         final Queue queue = QueueFactory.getQueue("default");
-
         TaskHandle th1 = queue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).tag("barfoo2").payload("foobar").etaMillis(15000));
         TaskHandle th2 = queue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).tag("barfoo2").payload("foofoo").etaMillis(10000));
-
-        List<TaskHandle> handles = queue.leaseTasksByTag(30, TimeUnit.MINUTES, 100, "barfoo2");
-        Assert.assertEquals(2, handles.size());
-        Assert.assertEquals(th2.getName(), handles.get(0).getName());
-        Assert.assertEquals(th1.getName(), handles.get(1).getName());
+        try {
+            List<TaskHandle> handles = queue.leaseTasksByTag(30, TimeUnit.MINUTES, 100, "barfoo2");
+            Assert.assertEquals(2, handles.size());
+            Assert.assertEquals(th2.getName(), handles.get(0).getName());
+            Assert.assertEquals(th1.getName(), handles.get(1).getName());
+        } finally {
+            queue.deleteTask(th1);
+            queue.deleteTask(th2);
+        }
     }
 
     @Test
     public void testPullMultipleWithDiffTag() throws Exception {
         final Queue queue = QueueFactory.getQueue("default");
-
         TaskHandle th1 = queue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).tag("barfoo3").payload("foobar").etaMillis(15000));
         TaskHandle th2 = queue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).tag("qwerty").payload("foofoo").etaMillis(10000));
         TaskHandle th3 = queue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).tag("barfoo3").payload("foofoo").etaMillis(10000));
+        try {
+            List<TaskHandle> handles = queue.leaseTasksByTag(30, TimeUnit.MINUTES, 100, "barfoo3");
+            Assert.assertEquals(2, handles.size());
+            Assert.assertEquals(th3.getName(), handles.get(0).getName());
+            Assert.assertEquals(th1.getName(), handles.get(1).getName());
 
-        List<TaskHandle> handles = queue.leaseTasksByTag(30, TimeUnit.MINUTES, 100, "barfoo3");
-        Assert.assertEquals(2, handles.size());
-        Assert.assertEquals(th3.getName(), handles.get(0).getName());
-        Assert.assertEquals(th1.getName(), handles.get(1).getName());
-
-        handles = queue.leaseTasksByTag(30, TimeUnit.MINUTES, 100, "qwerty");
-        Assert.assertEquals(1, handles.size());
-        Assert.assertEquals(th2.getName(), handles.get(0).getName());
+            handles = queue.leaseTasksByTag(30, TimeUnit.MINUTES, 100, "qwerty");
+            Assert.assertEquals(1, handles.size());
+            Assert.assertEquals(th2.getName(), handles.get(0).getName());
+        } finally {
+            queue.deleteTask(th1);
+            queue.deleteTask(th2);
+            queue.deleteTask(th3);
+        }
     }
 
     @Test
     public void testPullWithGroupTag() throws Exception {
         final Queue queue = QueueFactory.getQueue("default");
-
         TaskHandle th1 = queue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).tag("barfoo3").payload("foobar").etaMillis(15000));
-        queue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).tag("qwerty").payload("foofoo").etaMillis(11000));
+        TaskHandle th2 = queue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).tag("qwerty").payload("foofoo").etaMillis(11000));
         TaskHandle th3 = queue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).tag("barfoo3").payload("foofoo").etaMillis(10000));
-
-        LeaseOptions options = LeaseOptions.Builder.withLeasePeriod(1000L, TimeUnit.SECONDS).countLimit(100).groupByTag();
-        List<TaskHandle> handles = queue.leaseTasks(options);
-        Assert.assertEquals(2, handles.size());
-        Assert.assertEquals(th3.getName(), handles.get(0).getName());
-        Assert.assertEquals(th1.getName(), handles.get(1).getName());
+        try {
+            LeaseOptions options = LeaseOptions.Builder.withLeasePeriod(1000L, TimeUnit.SECONDS).countLimit(100).groupByTag();
+            List<TaskHandle> handles = queue.leaseTasks(options);
+            Assert.assertEquals(2, handles.size());
+            Assert.assertEquals(th3.getName(), handles.get(0).getName());
+            Assert.assertEquals(th1.getName(), handles.get(1).getName());
+        } finally {
+            queue.deleteTask(th1);
+            queue.deleteTask(th2);
+            queue.deleteTask(th3);
+        }
     }
 }
