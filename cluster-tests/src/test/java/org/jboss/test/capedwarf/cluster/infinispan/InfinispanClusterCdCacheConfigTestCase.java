cd03da4e21ed4e143e15207cf3af3526f5be8ffe
Matej Lazar <matejonnet@gmail.com>
2012-10-02 17:30:04 +0200

    Remove no CD cluster tests and rename all tests to *TestCase.

---
+++ cluster-tests/src/test/java/org/jboss/test/capedwarf/cluster/infinispan/InfinispanClusterCdCacheConfigTestCase.java
@@ -0,0 +1,42 @@
+package org.jboss.test.capedwarf.cluster.infinispan;
+
+import java.io.IOException;
+
+import org.infinispan.Cache;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.capedwarf.common.infinispan.CacheName;
+import org.jboss.capedwarf.common.infinispan.InfinispanUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Test Infinispan cache configured at runtime by CapeDwarf.
+ *
+ * @author Matej Lazar
+ */
+@RunWith(Arquillian.class)
+public class InfinispanClusterCdCacheConfigTestCase extends AbstractInfinispanClusterTest {
+
+    @Override
+    protected Cache<String, String> getCache() {
+        Cache<String, String> cache = InfinispanUtils.<String, String>getCache(CacheName.SEARCH);
+        return cache;
+    }
+
+    @InSequence(10)
+    @Test
+    @OperateOnDeployment("dep1")
+    public void wordCountTestCase() throws IOException {
+        wordCount();
+    }
+
+    @InSequence(1000)
+    @Test
+    @OperateOnDeployment("dep1")
+    public void cleanUp() throws IOException {
+        getCache().clear();
+    }
+
+}
--- cluster-tests/src/test/java/org/jboss/test/capedwarf/cluster/infinispan/InfinispanClusterXmlCacheConfigTestCase.java
+++ cluster-tests/src/test/java/org/jboss/test/capedwarf/cluster/infinispan/InfinispanClusterXmlCacheConfigTestCase.java
@@ -6,20 +6,19 @@ import javax.annotation.Resource;
 
 import org.infinispan.Cache;
 import org.infinispan.manager.CacheContainer;
-import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
-import org.jboss.arquillian.container.test.api.TargetsContainer;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.junit.InSequence;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
+ * Test Infinispan cache configuration used by CapeDwarf.
+ *
  * @author Matej Lazar
  */
 @RunWith(Arquillian.class)
-public class InfinispanClusterXmlCacheConfigTest extends AbstractInfinispanClusterTest {
+public class InfinispanClusterXmlCacheConfigTestCase extends AbstractInfinispanClusterTest {
 
     @Resource(lookup="java:jboss/infinispan/container/capedwarf")
     CacheContainer container;
@@ -34,24 +33,28 @@ public class InfinispanClusterXmlCacheConfigTest extends AbstractInfinispanClust
         return cache;
     }
 
+    /**
+     * Wake-up cache on second node.
+     */
     @InSequence(10)
+    @Test
+    @OperateOnDeployment("dep2")
+    public void wakeUpCache() throws IOException {
+        getCache();
+    }
+
+    @InSequence(20)
     @Test
     @OperateOnDeployment("dep1")
     public void wordCountTestCase() throws IOException {
         wordCount();
     }
 
-    @Deployment (name = "dep1") @TargetsContainer("container-1")
-    public static WebArchive getDeploymentA() {
-        return getDeployment();
+    @InSequence(1000)
+    @Test
+    @OperateOnDeployment("dep1")
+    public void cleanUp() throws IOException {
+        getCache().clear();
     }
 
-    @Deployment(name = "dep2") @TargetsContainer("container-2")
-    public static WebArchive getDeploymentB() {
-        return getDeployment();
-    }
-
-    public static WebArchive getDeployment() {
-        return getBaseDeployment();
-    }
 }
