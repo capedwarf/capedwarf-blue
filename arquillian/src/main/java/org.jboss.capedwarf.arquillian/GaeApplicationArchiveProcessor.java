69acb563ca680b803a04b85e40f0d6763835211c
Ales Justin <ales.justin@gmail.com>
2012-12-22 23:33:47 +0100

    Fix GAE jar check.

--- arquillian/src/main/java/org.jboss.capedwarf.arquillian/GaeApplicationArchiveProcessor.java
+++ arquillian/src/main/java/org.jboss.capedwarf.arquillian/GaeApplicationArchiveProcessor.java
@@ -22,22 +22,29 @@
 
 package org.jboss.capedwarf.arquillian;
 
+import java.io.File;
+import java.util.Set;
+
 import org.jboss.arquillian.container.test.spi.client.deployment.ApplicationArchiveProcessor;
 import org.jboss.arquillian.test.spi.TestClass;
 import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.Node;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-
-import java.io.File;
 
 /**
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
 public class GaeApplicationArchiveProcessor implements ApplicationArchiveProcessor {
     public void process(Archive<?> archive, TestClass testClass) {
-        final String className = testClass.getName();
-        if (className.contains(".testsuite.") && className.contains("JPA"))
-            return; // ignoring testsuite test classes, they already contain GAE API jar
+        final Node lib = archive.get("WEB-INF/lib");
+        if (lib != null) {
+            final Set<Node> libs = lib.getChildren();
+            for (Node jar : libs) {
+                if (jar.getPath().get().contains("appengine-api"))
+                    return;
+            }
+        }
 
         if (archive instanceof WebArchive) {
             WebArchive war = (WebArchive) archive;
