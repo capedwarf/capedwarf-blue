7e52fe8552ec85ee96fb4f7e4790b2d610d7fba4
Ales Justin <ales.justin@gmail.com>
2013-01-10 22:59:13 +0100

    Support max concurrent requests config.

--- common/src/main/java/org/jboss/capedwarf/common/singlethread/SingleThreadFilter.java
+++ common/src/main/java/org/jboss/capedwarf/common/singlethread/SingleThreadFilter.java
@@ -1,6 +1,7 @@
 package org.jboss.capedwarf.common.singlethread;
 
 import java.io.IOException;
+import java.util.concurrent.Semaphore;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -11,22 +12,36 @@ import javax.servlet.ServletResponse;
 
 /**
  * @author <a href="mailto:mluksa@redhat.com">Marko Luksa</a>
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
 public class SingleThreadFilter implements Filter {
+    private Semaphore semaphore;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        String mcr = filterConfig.getInitParameter("max-concurrent-requests");
+        int maxConcurrentRequests = (mcr != null) ? Integer.parseInt(mcr) : 1;
+        semaphore = new Semaphore(maxConcurrentRequests);
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
-        // make the app process one request at a time
-        synchronized (this) {
-            chain.doFilter(servletRequest, servletResponse);
+        try {
+            // make the app process one request at a time
+            semaphore.acquire();
+            try {
+                chain.doFilter(servletRequest, servletResponse);
+            } finally {
+                semaphore.release();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new IOException(e);
         }
     }
 
     @Override
     public void destroy() {
+        semaphore = null;
     }
 }
