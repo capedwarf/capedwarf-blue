b53cb376beb8ea77e6941e689ddd873e5a8fcacb
Ales Justin <ales.justin@gmail.com>
2014-07-28 15:44:41 +0200

    Remove F8 code.

--- blobstore/src/test/java/org/jboss/test/capedwarf/blobstore/support/FileUploader.java
+++ blobstore/src/test/java/org/jboss/test/capedwarf/blobstore/support/FileUploader.java
@@ -22,25 +22,19 @@
 
 package org.jboss.test.capedwarf.blobstore.support;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.jar.JarEntry;
-import java.util.jar.JarOutputStream;
 
-import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.ByteArrayBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.jboss.capedwarf.common.io.Base64Utils;
 
 /**
  * @author <a href="mailto:mluksa@redhat.com">Marko Luksa</a>
@@ -78,60 +72,4 @@ public class FileUploader {
         return uploadFile(uri, partName, "", "application/octet-stream", new byte[0]);
     }
 
-    public String putFile(String uri, String partName, String filename, String mimeType, byte[] contents, String location) throws URISyntaxException, IOException {
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpPut put = new HttpPut(uri);
-            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-            ByteArrayBody contentBody = new ByteArrayBody(contents, ContentType.create(mimeType), filename);
-            builder.addPart(partName, contentBody);
-            put.setEntity(builder.build());
-
-            put.addHeader("Authorization", "BASIC " + Base64Utils.encode("admin:admin".getBytes()));
-
-            if (location != null) {
-                put.addHeader("X-Location", location);
-            }
-
-            HttpResponse response = httpClient.execute(put);
-            Header header = response.getFirstHeader("X-Location");
-            return (header != null) ? header.getValue() : null;
-        }
-    }
-
-    private static void addEntry(JarOutputStream jas, String name, byte[] content) throws Exception {
-        JarEntry entry = new JarEntry(name);
-        jas.putNextEntry(entry);
-        if (content != null) {
-            jas.write(content);
-        }
-        jas.closeEntry();
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        JarOutputStream jas = new JarOutputStream(baos);
-
-        addEntry(jas, "hello.txt", null);
-
-        /*
-        Properties properties = new Properties();
-        properties.setProperty("groupId", "org.acme");
-        properties.setProperty("artifactId", "acme-core");
-        properties.setProperty("version", "1.0");
-        ByteArrayOutputStream b1 = new ByteArrayOutputStream();
-        properties.store(b1, null);
-        addEntry(jas, "META-INF/maven/org.acme/acme-core/1.0/pom.properties", b1.toByteArray());
-        */
-
-        jas.close();
-
-        final FileUploader uploader = new FileUploader();
-
-        String location = uploader.putFile("http://localhost:8181/maven/upload/acme-core-1.0.jar", "file", "acme-core-1.0.jar", "application/octet-stream", baos.toByteArray(), null);
-        System.out.println("Location = " + location);
-
-        location = uploader.putFile("http://localhost:8181/maven/upload/org.acme/acme-core/1.0/acme-core.jar", "file", "acme-core-1.0.jar", "application/octet-stream", baos.toByteArray(), location);
-        System.out.println("Location = " + location);
-    }
 }
